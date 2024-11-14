e4e28a522d698944b24881128a67604279b3ef66
Daniil Yashin <luckyman1223@mail.ru>
2024-11-14 19:02:05 +0300

    изменена система нахождения нерешенных частей картинки

--- detect_field.py
+++ detect_field.py
@@ -13,35 +13,21 @@ def detect_picture_field(image):
     crop = image[y:y+h, x:x+w]
     mask = cv2.inRange(crop, (250, 250, 250), (255, 255, 255))
 
-    square = False
-    aspect_ratio = float(w)/h
-    if 0.9 <= aspect_ratio <= 1.1:
-            square = True
-
     # Ищем контуры квадратиков внутри, чтобы определить размер (15*15 или 20*20)
     contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     square_contours = []
     for cnt in contours:
-        x, y, w, h = cv2.boundingRect(cnt)
-        aspect_ratio = float(w)/h
+        cnt_x, cnt_y, cnt_w, cnt_h = cv2.boundingRect(cnt)
+        aspect_ratio = float(cnt_w)/cnt_h
         area = cv2.contourArea(cnt)
         # Проверяем что контур примерно квадратный (соотношение сторон близко к 1)
         if 0.95 <= aspect_ratio <= 1.05 and area > 1000 and area < 20000:
-            square_contours.append(cnt)
-            # Рисуем точку в центре квадрата
-            center_x = x + w // 2
-            center_y = y + h // 2
-            cv2.circle(crop, (center_x, center_y), 5, (255, 0, 255), -1)
-            cv2.imshow('crop', crop)
-            cv2.waitKey(0)
-            print(area)
+            # Рисуем точку в центре каждого квадрата
+            center_x = cnt_x + cnt_w // 2
+            center_y = cnt_y + cnt_h // 2
+            square_contours.append([x+center_x, y+center_y])
 
-    if square:
-        shape = int(len(square_contours)**0.5)
-    else:
-        shape = None
-
-    return {'coords':[[x, x+w], [y, y+h]], 'shape':shape} 
+    return square_contours
 
 
 def puzzle_mask(list_of_masks):
@@ -55,36 +41,43 @@ def puzzle_mask(list_of_masks):
 
 def detect_puzzle(image):
 
+    # Создаем маску для рассчета размерности паззла
+    # for DeerMyFriend
+    mask_dmf = cv2.inRange(image, (70, 47, 39), (255, 255, 255))
+    # mask = cv2.inRange(image, (68, 47, 36), (255, 255, 255))
+    # mask = cv2.inRange(image, (60, 37, 30), (255, 255, 255))
+    # for normal
+    mask_normal = cv2.inRange(image, (95, 69, 66), (255, 255, 255))
+    # for big
+    mask_big = cv2.inRange(image, (108, 93, 17), (255, 255, 255))
+    mask = puzzle_mask([mask_dmf, mask_normal, mask_big])
+
     # Создаем маску для основного поля
-    field_mask = cv2.inRange(image, (90, 90, 90), (255, 255, 255))
+    # field_mask = cv2.inRange(image, (90, 90, 90), (255, 255, 255))
     # Ищем контуры основного поля
-    contours, _ = cv2.findContours(field_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     field_contour = max(contours, key=cv2.contourArea)
 
     x, y, w, h = cv2.boundingRect(field_contour)
-    # field = detect_field(image)
 
     # Обрезаем по основному полю
-    crop = image[y:y+h, x:x+w]
+    crop = mask[y:y+h, x:x+w]
+    cv2.imshow('crop', crop)
+    # cv2.imshow('crop', cv2.resize(crop, None, fx=0.45, fy=0.45))
+    cv2.waitKey(0)
 
-    # crop = cv2.blur(crop, (5, 5))
-    # Создаем маску для рассчета размерности паззла
-    # for DeerMyFriend
-    mask_dmf = cv2.inRange(crop, (70, 47, 39), (255, 255, 255))
-    # mask = cv2.inRange(crop, (68, 47, 36), (255, 255, 255))
-    # mask = cv2.inRange(crop, (60, 37, 30), (255, 255, 255))
-    # for normal
-    mask_normal = cv2.inRange(crop, (95, 69, 66), (255, 255, 255))
-    # for big
-    mask_big = cv2.inRange(crop, (108, 93, 17), (255, 255, 255))
-    mask = puzzle_mask([mask_dmf, mask_normal, mask_big])
+    cnt = cv2.findContours(crop, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    cv2.drawContours(image[y:y+h, x:x+w], cnt, -1, (255, 0,255), 2)
+    cv2.imshow('image', cv2.resize(image, None, fx=0.45, fy=0.45))
+    cv2.waitKey(0)
 
+    print(len(cnt))
 
     return 
 
 
 from take_frame import frame
 
-field = detect_picture_field(frame())
+field = detect_puzzle(frame())
 print(field)
 
